1582c4488d797e1a7075a35a066ee106b13747d7
Brynfor Morris <brynfor.morris@gmail.com>
2023-04-17 16:01:46 -0500

    uploaded sample seed file

--- server/seed.py
+++ server/seed.py
@@ -10,10 +10,32 @@ from faker import Faker
 from app import app
 from models import db
 
+from models import db, Many1, Many2, SampleJoin
 
+with app.app_context():
 
-if __name__ == '__main__':
-    fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+    faker = Faker()
+
+    print("Deleting data...")
+    Many1.query.delete()
+    Many2.query.delete()
+    SampleJoin.query.delete()
+
+    print("Creating Many1s...")
+
+    Many1s_list = [Many1(topic = faker.sentence(), year = faker.year(), page_count = random.randint(1,10)) for _ in range(1,20)]
+
+    print("Creating Many2s...")
+
+    Many2s_list = [Many2(topic = faker.sentence(), year = faker.year(), page_count = random.randint(1,10)) for _ in range(1,20)]
+
+    print("Creating SampleJoins...")
+
+    SampleJoin_list = [SampleJoin(many1_id = random.randint(1,20), many2_id = random.randint(1,20)) for _ in range(1,30)]
+    
+    db.session.add_all(Many2s_list)
+    db.session.add_all(Many1s_list)
+    db.session.add_all(SampleJoin_list)
+    db.session.commit()
+
+    print("Seeding done!")

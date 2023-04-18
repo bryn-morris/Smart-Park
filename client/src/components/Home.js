05ba9b4ed9d5a2151770fd4bf319611579960d70
Brynfor Morris <brynfor.morris@gmail.com>
2023-04-18 15:26:25 -0500

    added minor home changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Home() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
-    <div>Home</div>
+    <div>
+      <div>Smart Parks (Project Title)</div>
+      <img 
+        src = 'https://images.unsplash.com/photo-1514373941175-0a141072bbc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80' 
+        alt = 'Dog Playing with Ball'/>
+      <button 
+        type='button'
+        onClick ={()=>{setIsModalOpen(true)}}
+      >
+        Check In!
+      </button>
+      
+    </div>
   )
 }
 

6e786662b5a9673a70b6ce6aad30e5b95e479411
Beau Kim <beaukim3@gmail.com>
2023-04-19 14:59:57 -0400

    WIP password hashing

--- server/models.py
+++ server/models.py
@@ -14,7 +14,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String)
     # Password hash
-    _password_hash = db.Column(db.String)
+    _password = db.Column(db.String)
     image = db.Column(db.String)
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
@@ -22,20 +22,22 @@ class User(db.Model, SerializerMixin):
 
     dogs = db.relationship('Dog', back_populates = 'user', cascade="all, delete-orphan")
 
+    # hybrid allows the password_hash to be accessed 
+    # by sqlalchemy queries as well as python
     @hybrid_property
     def password_hash(self):
-        return self._password_hash
+        return self._password
 
     @password_hash.setter
-    def password_hash(self, password):
+    def password_hash(self, password=""):
 
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password = password_hash.decode('utf-8')
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+            self._password, password.encode('utf-8'))
 
 class Dog(db.Model, SerializerMixin):
 
@@ -68,7 +70,7 @@ class Visit(db.Model, SerializerMixin):
     
     id = db.Column(db.Integer, primary_key=True)
     length_of_stay = db.Column(db.Integer)
-    actual_length_of_stay = db.Column(db.Integer)
+    # actual_length_of_stay = db.Column(db.Integer)
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())

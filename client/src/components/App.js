c29024b18e9704c4f0909f05a7f06dfebde44bc8
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-04-19 09:23:36 -0400

    patch working

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,16 +4,43 @@ import Home from "./Home";
 import CheckIn from "./CheckIn";
 import Header from "./Header"
 import DogPark from "./DogPark"
+import MyAccount from "./MyAccount"
 
 function App() {
 
   const [dogParks, setDogParks] = useState([])
-
   useEffect(()=>{
     fetch('http://127.0.0.1:5555/dogparks')
       .then(r=> r.json())
       .then(setDogParks)
   }, [])
+
+  const [dogs, setDogs] = useState([])
+  useEffect(()=>{
+    fetch('http://127.0.0.1:5555/dogs')
+      .then(r=> r.json())
+      .then(setDogs)
+  }, [])
+
+  const showRemainingDogs = (id) => {
+    const newDogArray = dogs.filter(dogObj => {
+      if(dogObj.id !== id)
+      {
+        return true
+      }
+    })
+    setDogs(newDogArray)
+  }
+
+  const updatedDogs = (editedDog) => {
+    const changedDogArr = dogs.map(dog => {
+      return (dog.id !== editedDog.id ? dog : editedDog)
+    })
+    setDogs(changedDogArr)
+  }
+
+  
+
 
   return (
     <div>
@@ -25,6 +52,9 @@ function App() {
           </Route>
           <Route exact path="/dogparks">
             <DogPark dogParks = {dogParks}/>
+          </Route>
+          <Route exact path="/myaccount">
+            <MyAccount dogs = {dogs} showRemainingDogs = {showRemainingDogs} updatedDogs = {updatedDogs}/>
           </Route>
           <Route exact path="/checkin">
             <CheckIn/>
---
+++ client/src/components/MyDogCards.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+
+function MyDogCards({id, name, breed, weight, age, image, showRemainingDogs, updatedDogs}) {
+    const [editForm, setEditForm] = useState(false)
+    const [dogAttribute, setDogAttribute]= useState('')
+    const [newInfo, setNewInfo] = useState('')
+    const handleAttributeChange = e => setDogAttribute(e.target.value)
+    const handleNewInfo = e => setNewInfo(e.target.value)
+    
+    const handleEditForm = () => {
+        setEditForm(!editForm)
+      }
+
+    const deleteDog = () => {
+        fetch (`http://127.0.0.1:5555/dogs/${id}`, {method:"DELETE"})
+            (showRemainingDogs(id))
+        }
+
+
+    const handleDogEdit = (e) => {
+        e.preventDefault()
+    
+        fetch (`http://127.0.0.1:5555/dogs/${id}`, {
+                method:"PATCH", 
+                headers:{"Content-type":"application/json"}, 
+                body: JSON.stringify({
+                    [dogAttribute]: newInfo,
+                })
+                })
+            .then (r => r.json())
+            .then(editedDog => updatedDogs(editedDog))
+
+            setNewInfo('')
+            setDogAttribute('')
+        }
+
+
+  return (
+    <div>
+        <img src = {image}/>
+        <h1>{name}</h1>
+        <h2>{breed}</h2>
+        <h3>{weight}</h3>
+        <h3>{age}</h3>
+        
+        {editForm ? (
+            <form onSubmit={handleDogEdit}>
+                <h3>Edit Dog</h3>
+                <select onChange={handleAttributeChange} value={dogAttribute}>
+                    <option>What to edit</option>
+                    <option>name</option>
+                    <option>weight</option>
+                    <option>age</option>
+                    <option>image</option>
+                    <option>breed</option>
+                </select>
+                <br />
+                <input type="text" placeholder="New info..." onChange={handleNewInfo} value={newInfo} />
+                <button type="submit" className="primary" onClick={console.log('hello')}>Done</button>
+            </form>
+            ) : (
+                <button onClick={handleEditForm}> Edit Dog</button>
+            )
+        }
+
+        <button onClick={deleteDog}>X</button>
+    </div>
+  )
+}
+
+export default MyDogCards

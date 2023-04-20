e7c7f951657d2fa8efc35af26a4ad46b8b3ba5ce
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-04-20 15:38:22 -0400

    started working on login page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SignUp from './SignUp'
 import LogIn from './LogIn'
 
 function App() {
+    const [signUp, setSignUp] = useState(false)
+    const [login, setLogin] = useState(false)
+  
+    const handleSignupClick=() =>{
+        setSignUp(!signUp)
+    }
+    const handleLoginClick=() =>{
+      setLogin(!login)
+  }
   return (
     <div>
-        <SignUp/>
-        <LogIn/>
+      <h1>Welcome to SmartPark!</h1>
+      
+        {signUp ? (
+          <div>
+            <SignUp/>
+            <button onClick={handleSignupClick}>Hide Signup</button>
+          </div>
+        ):(
+          <button onClick={handleSignupClick}>Create Account</button>
+        )}
+
+        {login ?(
+          <div>
+            <LogIn/>
+            <button onClick={handleLoginClick}>Hide LogIn</button>
+          </div>
+        ):(
+          <button onClick={handleLoginClick}>Login</button>
+        )}
+
     </div>
   )
 }

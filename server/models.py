8748e1401cc594c18198c51b3e9271e158bd04e1
Brynfor Morris <brynfor.morris@gmail.com>
2023-04-20 05:20:50 -0500

    test

--- server/models.py
+++ server/models.py
@@ -38,6 +38,12 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password, password.encode('utf-8'))
+    
+    @validates('_password')
+    def pass_hashing(self, key, attr):
+
+        password_hash = bcrypt.generate_password_hash(attr.encode('utf-8'))
+        return password_hash.decode('utf-8')
 
 class Dog(db.Model, SerializerMixin):
 

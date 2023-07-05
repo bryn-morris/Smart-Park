a5d5906338f007b365a368168863e047ce01464b
Brynfor Morris <brynfor.morris@gmail.com>
2023-07-05 15:21:30 -0500

    making styling consistent

--- client/src/components/DogPark/SearchDogPark.js
+++ client/src/components/DogPark/SearchDogPark.js
@@ -4,13 +4,13 @@ function SearchDogPark ({specificPark}) {
     return(
         <div className="ui large search" style={{float:'right', margin:20}}>
             <div className="ui icon input">
-                    <input
+                <input
                     className="prompt"
                     type="text"
                     placeholder="Search dog park name..."
                     onChange={(e) => specificPark(e.target.value)}
-                    />
-                    <i className="search icon"></i>
+                />
+                <i className="search icon"></i>
             </div>
         </div>
     )

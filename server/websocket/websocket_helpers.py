112340354c48496ed6ca507bbe078ae1a27a8c81
Brynfor Morris <brynfor.morris@gmail.com>
2023-09-29 13:29:40 -0500

    streamlined websocket helper code

--- server/websocket/websocket_helpers.py
+++ server/websocket/websocket_helpers.py
@@ -14,49 +14,39 @@ def check_rooms(room_name):
 
 def join_user_to_room(self, user_id):
     
-    room_name = f'user_{user_id}'
+    self.room_name = f'user_{user_id}'
+    join_room(self.room_name)
 
-    self.room_name = room_name
-
-    join_room(room_name)
-
-    ## if room doesn't already exist in db, add to db
-    if not check_rooms(room_name):
-
+    if not check_rooms(self.room_name):
         try:
-            db.session.add(WebSocket_Rooms(room_name = room_name, user_id = user_id))
-            db.session.commit()
+            self.active_room = WebSocket_Rooms(room_name = self.room_name, user_id = user_id)
+            db.session.add(self.active_room)
+            db.session.commit() 
         except:
             WebSocketAuthenticationError('Room name is not unique!')
 
 def emit_message_to_room(self, event_name, data_dict, room_name):
-   
-    ## add room_name to self.room_name
-    active_room = check_rooms(room_name)
 
-    if not active_room:
+    if not self.active_room:
         raise WebSocketAuthenticationError('No websocket room exists for this user!')
 
-    self.emit(event_name, data_dict, room = active_room)
+    self.emit(event_name, data_dict, room = self.active_room)
 
-def remove_user_from_room(room_name, user_id):
+def remove_user_from_room(self, room_name, user_id):
 
-    active_room = check_rooms(room_name)
     active_user = User.query.filter(User.id == user_id).first()
 
-    if active_room and active_room.user == active_user:
+    if self.active_room and self.active_room.user == active_user:
         leave_room(room_name)
         
+def close_room(self, room_name):
 
-def close_room(room_name):
-
-    if check_rooms(room_name):
+    if self.active_room:
         close_room(room_name)
-    
-    ## del self.room_id
-    ## check to make sure room exists within active room database
-    ## if so, close room and delete from database
-    pass
+        del self.room_id
+        db.session.delete(self.active_room)
+        db.session.commit()
+
 
 def diconnect_user(user_id):
     ## del self.user_id

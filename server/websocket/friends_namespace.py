8ca4a9e6058c39b0509d3780b4c5cb6320be99d4
Brynfor Morris <brynfor.morris@gmail.com>
2023-10-10 12:31:24 -0500

    removed redundant pseudocode in friends_namespace

--- server/websocket/friends_namespace.py
+++ server/websocket/friends_namespace.py
@@ -57,12 +57,8 @@ class FriendNamespace(Namespace):
             raise ValueError('You can\'t add yourself as a friend!')
 
         ## return error if user is already in pending friends list
-        if sel_friend in (sel_user.pend_friends_1.all()+sel_user.pend_friends_2.all()):
+        if sel_friend in sel_user.pend_friends_1.all() or sel_friend in sel_user.pend_friends_2.all():
             raise ValueError('You have already sent a friend request to this user!')
-
-
-        ## First, User A will send Friend Request to User B
-        ## This will to add both users to pending friendships table
 
         new_pend_fr = Pending_Friendships(
                 pend_friend_1_id = user_id,
@@ -71,10 +67,6 @@ class FriendNamespace(Namespace):
         
         db.session.add(new_pend_fr)
         db.session.commit()
-
-        ## HANDLE THIS WITH POSTGRES
-        ## Add contraint to pending friendships table such that only one pairing can be present at a time, 
-        ## in either direction, this will cause an error that will get picked up by the global on_error handler
 
         ## emit message to room of both users a and b that updates pending friend request state
         
@@ -109,63 +101,3 @@ class FriendNamespace(Namespace):
         ### If User A cancels, remove users from pending friendships table
         ### If User B accepts, users will be removed from pending friendships table and be added to the friendships table
         ### Once Friendship is established, either user can delete friendship to remove from friendship table
-
-############################################################
-#########              Friendship Decorator
-############################################################
-
-# def Friendship_Decorator(func):
-
-#     def wrapper_func(*args, **kwargs):
-        
-#         data = request.json()
-#         current_user = User.query.filter(User.id == session['user_id']).one()
-#         sel_friend = User.query.filter(User.id == data['friend_id']).one()
-
-#         if sel_friend not in current_user.all_friends():
-#             ## add an entry in the pending_friends table
-
-#             new_pend_fr = Pending_Friendships(
-#                 pend_friend_1_id = current_user.id,
-#                 pend_friend_2_id = data['friend_id']
-#             )
-
-#             db.session.add(new_pend_fr)
-#             db.session.commit()
-
-#             ## Then send a response to the current user stating request sent
-#             ## and include the pending friend attribute
-
-#             resp_body = new_pend_fr.to_dict()
-#             resp_body['request_status'] = False
-
-#             return make_response(resp_body,201)
-            
-#             ## Then check if other friend user is logged into application, if so
-#             ## send a response to the other user that has had their
-#             ## User Model Updated with an updated pending friend attribute
-#             ## and then process some sort of notification from that
-            
-#             ## can maybe use socket.io to establish this connection?
-#             ## look into websockets and server-side events
-
-#             ## If Flask keeps track of all the possible app contexts on the backend somehwere
-#             ## we could maybe sort through those to find one in which 
-#             ## the session user id key matches the friend id
-#             ## and then work from there?
-#             pass
-
-#         elif data['request_status'] == True:
-
-#             ## grab friendship data from request
-#             ## remove any mention of each of these users from each of their
-#             ## pending friend request User Attributes
-#             ## then return func so that they can be added to friendship
-#             ## table below
-#             return func(*args, **kwargs)
-        
-#         else:
-#             #This shouldn't ever trip but adding for redundancy
-#             return make_response({"error" : "You are already friends with this User!"}, 400)
-    
-#     return wrapper_func

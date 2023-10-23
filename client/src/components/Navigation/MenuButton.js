2312479683f7a57f391db82d9fcb00e8ae107cf4
Brynfor Morris <brynfor.morris@gmail.com>
2023-10-23 03:14:58 -0500

    working on refactoring scss

--- client/src/components/Navigation/MenuButton.js
+++ client/src/components/Navigation/MenuButton.js
@@ -1,13 +1,18 @@
-
+import { useState } from 'react'
 
 function MenuButton () {
 
+    const [isActive, setIsActive] = useState(false)
+
+    function toggleButton () {
+        setIsActive(!isActive)
+    }
+
     return(
         <div className="menuButtonContainer">
-            <div className = "menuToggle">
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
+            <div className = 'navButton' onClick={toggleButton}>
+                <div className='icon-left'></div>
+                <div className = 'icon-right'></div>
             </div>
         </div>
     )

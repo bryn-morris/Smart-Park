abc2ad0afb85028a231dff1d7bd6e5c7b5464588
Brynfor Morris <brynfor.morris@gmail.com>
2023-10-27 11:33:40 -0500

    developed pseudocode further for navmenu styling

--- client/src/components/Navigation/NavMenu.js
+++ client/src/components/Navigation/NavMenu.js
@@ -5,10 +5,11 @@ function NavMenu({isActive, toggleButton}){
     const menuLabels = ["Profile","Checkout"]
 
     const menuIcons = ["Logout", "Settings"]
+    // Menu Icons are going to need a new component similar to head
     // Instead of having lougout text and settings text
     // have a subdivided menu towards the top that just contains
-    // a settings icon and a logout icon that 
-    // highlight hwen you hover over them and have a tooltip
+    // a settings icon and a logout icon that sits at the bottom of the menu
+    // highlight when you hover over them and have a tooltip
 
     function generateMenuItems (menuLabels) {
 

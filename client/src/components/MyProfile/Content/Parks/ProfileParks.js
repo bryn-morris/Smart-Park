b1efd098cd993095654df69457fd2a83d9f979e0
Brynfor Morris <brynfor.morris@gmail.com>
2023-11-14 14:48:26 -0600

    added logic for incrementing and decreasing fav parks count obj to slice

--- client/src/components/MyProfile/Content/Parks/ProfileParks.js
+++ client/src/components/MyProfile/Content/Parks/ProfileParks.js
@@ -1,6 +1,40 @@
+import { DogParkContext } from "../../../../context/DogParkContext"
 import FavoriteParks from "./FavoriteParkListEntry"
+import { useState, useContext } from "react"
+
 
 function ProfileParks () {
+
+    const {favoritedParksByUser} = useContext(DogParkContext)
+
+    const initialCountObj = {
+        startIndex : 0,
+        endIndex : 5,
+    }
+
+    const [countObj, setCountObj] = useState(initialCountObj)
+    const [visibleFavoriteParks, setVisibleFavoriteParks] = useState(favoritedParksByUser.slice(countObj["startIndex"], countObj["endIndex"]))
+
+    function incrementFavParks () {
+        if (countObj.endIndex !== favoritedParksByUser.length + 1) {
+            setCountObj((prevState)=>{return({
+                ...prevState,
+                startIndex: prevState.startIndex + 1,
+                endIndex: prevState.endIndex + 1,
+            })})
+        }}
+
+    function decrementFavParks () {
+        if(countObj.startIndex !== 0){
+            setCountObj((prevState)=>{return({
+                ...prevState,
+                startIndex: prevState.startIndex - 1,
+                endIndex: prevState.endIndex - 1,
+            })})
+        }
+
+    }
+
     return(
         <div className = "parksContainer">
             <div className = "favoritesContainer">

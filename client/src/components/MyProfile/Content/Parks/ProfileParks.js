8fe8f121b4a4009d666a00fb727c3c3442b96027
Brynfor Morris <brynfor.morris@gmail.com>
2023-11-14 08:13:44 -0600

    created scaffolding for scrollable container and parks within profile routing

--- client/src/components/MyProfile/Content/Parks/ProfileParks.js
+++ client/src/components/MyProfile/Content/Parks/ProfileParks.js
@@ -1,10 +1,21 @@
-
+import FavoriteParks from "./FavoriteParkListEntry"
 
 function ProfileParks () {
     return(
-        <div>
-            ProfileParks
+        <div className = "parksContainer">
+            <div className = "favorites">
+                favoriteparks
+                {/* <FavoriteParks /> */}
+            </div>
+            <div className = "recents">
+                reccentparks
+            </div>
+            <div className = "reviews">
+                reviewedparks
+            </div>
         </div>
+
+        
     )
 }
 
